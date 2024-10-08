2e26ced6afb4b19a69c3d6585bec261b1b30fe1f
arangrhie <arrhie@gmail.com>
2024-10-08 10:28:01 -0400

    map only option

--- variants_sr/_submit_extract_map_variant_sr.sh
+++ variants_sr/_submit_extract_map_variant_sr.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [[ "$#" -lt 2 ]]; then
-  echo "Usage: ./_submit_extract_map_variant_sr.sh sampleInfo.txt sampleIdx"
+  echo "Usage: ./_submit_extract_map_variant_sr.sh [-maponly] sampleInfo.txt sampleIdx"
   echo "  sampleInfo.txt  Sample information table."
   echo "                  [sampleIdx] [sampleId] [path/to/original/bam]"
   echo "  sampleIdxs      Sample index to submit this pipeline."
   echo "                  e.g. 1-30 for samples from 1 to 30 in sampleInfo.txt"
+  echo "  -maponly        extract, map and stop. OPTIONAL"
   exit -1
 fi
 
@@ -17,6 +18,12 @@ PIPELINE=$tools/T2T-Ref
 refWiY=$PIPELINE/ref/chm13v2.0_masked_DJ_5S_rDNA_PHR_hardMaskY.fa
 refWoY=$PIPELINE/ref/chm13v2.0_masked_DJ_5S_rDNA_PHR_noY.fa
 
+maponly="N"
+if [ "x$1" = "x-maponly" ]; then
+  maponly="Y"
+  shift
+fi
+
 sampleInfo=$1
 sampleIndex=$2
 
@@ -25,7 +32,13 @@ array="--array=$2"		# Arguement passed on for --array. e.g. 1-30 for "--array=1-
 ## Submission env variables
 mkdir -p logs
 name=srWGS_wrapper
-script=$PIPELINE/variants_sr/extract_map_calcov_dv.sh
+
+if [[ $maponly == "Y" ]]; then
+  script=$PIPELINE/variants_sr/extract_map.sh
+else
+  script=$PIPELINE/variants_sr/extract_map_calcov_dv.sh
+fi
+
 args="$sampleInfo $refWiY $refWoY"
 log=logs/$name.%A_%a.log
 cpus=24
